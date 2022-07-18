220c98e1bdd6a9f3829a303fdeac420470ce4c6f
Aine <aine@etke.cc>
2022-07-18 18:46:49 +0300

    fix versions parser

--- bin/commit-msg.sh
+++ bin/commit-msg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-grep -rhE "^matrix_.*_version: |^custom_.*_version: |^int_.*_version: " ./upstream/roles/*/defaults/main.yml ./roles/*/*/defaults/main.yml | sed -e "s/matrix_//;s/custom_//;s/int_//;s/_version//;/^synapse_default/d;/^synapse_ext/d;/^mailer_container/d;s/bot_//;s/client_//;s/mautrix_//" | sort | yq eval -M -P | sed "s/^/\*\ /" > $PWD/VERSIONS.md
+grep -rhE "^matrix_.*_version: |^custom_.*_version: |^int_.*_version: " ./upstream/roles/*/defaults/main.yml ./roles/*/*/defaults/main.yml | grep -Ev '{{|master|""' | sed -e "s/matrix_//;s/custom_//;s/int_//;s/_version//;/^synapse_default/d;/^synapse_ext/d;/^mailer_container/d;s/bot_//;s/client_//;s/mautrix_//" | sort | yq eval -M -P | sed "s/^/\*\ /" > $PWD/VERSIONS.md
 
 PREFIX_OLD="\-\*"
 PREFIX_NEW="\+\*"
