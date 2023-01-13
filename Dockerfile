etkecc/ansible

commit 9567b8716c1440b61a79f12c43048e4184880736

optimize Dockerfile

Dockerfile
@@ -16,7 +16,6 @@ RUN apk --no-cache add git make skopeo && \
 
 FROM registry.gitlab.com/etke.cc/ansible/base
 WORKDIR /playbook
-ENTRYPOINT ["/bin/sh"]
-RUN apk add --no-cache ca-certificates openssh git npm hugo make
+RUN apk add --no-cache ca-certificates openssh git hugo make
 COPY --from=playbook /playbook /playbook
 COPY --from=tools /bin/emm /bin/emm

commit b31f4f5181e3f1d952dc9c0e07912a0dfca47c14

Restore ENTRYPOINT in container image
Regression since 9567b8716c1440b61a79f12

Dockerfile
@@ -16,6 +16,7 @@ RUN apk --no-cache add git just && \
 
 FROM registry.gitlab.com/etke.cc/ansible/base
 WORKDIR /playbook
+ENTRYPOINT ["/bin/sh"]
 RUN apk add --no-cache ca-certificates openssh git hugo make just skopeo
 COPY --from=playbook /playbook /playbook
 COPY --from=tools /bin/emm /bin/emm