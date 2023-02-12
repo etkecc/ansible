8f9f4cc5174e11ddbabf74dfcc252e8405e666fc
Aine <aine@etke.cc>
2023-02-12 22:06:22 +0200

    add callback plugin

---
+++ plugins/callback/etkeline.py
@@ -0,0 +1,117 @@
+# Modified version of ansible oneline callback plugin
+# source: https://github.com/ansible/ansible/blob/devel/lib/ansible/plugins/callback/oneline.py
+# (c) 2012-2014, Michael DeHaan <michael.dehaan@gmail.com>
+# (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+    name: etkeline
+    type: stdout
+    short_description: modified oneline Ansible screen output
+    version_added: historical
+    description:
+        - The original plugin is the output callback used by the -o/--one-line command line option.
+'''
+
+from ansible import constants as C
+from ansible.plugins.callback import CallbackBase
+from ansible.utils.color import colorize, hostcolor
+
+
+class CallbackModule(CallbackBase):
+
+    '''
+    This is the default callback interface, which simply prints messages
+    to stdout when new callback events are received.
+    '''
+
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'etkeline'
+
+    def _command_generic_msg(self, hostname, result, caption):
+        stdout = result.get('stdout', '').replace('\n', '\\n').replace('\r', '\\r')
+        task_name = result._task.get_name().strip()
+        if 'stderr' in result and result['stderr']:
+            stderr = result.get('stderr', '').replace('\n', '\\n').replace('\r', '\\r')
+            return "[%s] %s | %s | rc=%s | (stdout) %s (stderr) %s" % (hostname, caption, task_name, result.get('rc', -1), stdout, stderr)
+        else:
+            return "[%s] %s | %s | rc=%s | (stdout) %s" % (hostname, caption, task_name, result.get('rc', -1), stdout)
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        if 'exception' in result._result:
+            if self._display.verbosity < 3:
+                # extract just the actual error message from the exception text
+                error = result._result['exception'].strip().split('\n')[-1]
+                msg = "An exception occurred during task execution. To see the full traceback, use -vvv. The error was: %s" % error
+            else:
+                msg = "An exception occurred during task execution. The full traceback is:\n" + result._result['exception'].replace('\n', '')
+
+            if result._task.action in C.MODULE_NO_JSON and 'module_stderr' not in result._result:
+                self._display.display(self._command_generic_msg(result._host.get_name(), result._result, '✖'), color=C.COLOR_ERROR)
+            else:
+                self._display.display(msg, color=C.COLOR_ERROR)
+
+        self._display.display("[%s] x | %s => %s" % (result._host.get_name(), result._task.get_name().strip(), self._dump_results(result._result, indent=2).replace("\\\\r\\\\n", "\n")),
+                              color=C.COLOR_ERROR)
+
+    def v2_runner_on_ok(self, result):
+
+        if result._result.get('changed', False):
+            color = C.COLOR_CHANGED
+            state = '+'
+        else:
+            color = C.COLOR_OK
+            state = '✓'
+
+        msg = "[%s] %s | %s" % (result._host.get_name(), state, result._task.get_name().strip())
+        if self._run_is_verbose(result):
+            msg += " => %s" % (self._dump_results(result._result, indent=2))
+        self._display.display(msg, color=color)
+
+    def v2_runner_on_unreachable(self, result):
+        self._display.display("[%s] ☠ => %s" % (result._host.get_name(), result._result.get('msg', '')), color=C.COLOR_UNREACHABLE)
+
+    def v2_runner_on_skipped(self, result):
+        pass
+
+    def v2_playbook_on_stats(self, stats):
+        self._display.banner("PLAY RECAP")
+
+        hosts = sorted(stats.processed.keys())
+        for h in hosts:
+            t = stats.summarize(h)
+
+            self._display.display(
+                u"%s : %s %s %s %s %s %s %s" % (
+                    hostcolor(h, t),
+                    colorize(u'ok', t['ok'], C.COLOR_OK),
+                    colorize(u'changed', t['changed'], C.COLOR_CHANGED),
+                    colorize(u'unreachable', t['unreachable'], C.COLOR_UNREACHABLE),
+                    colorize(u'failed', t['failures'], C.COLOR_ERROR),
+                    colorize(u'skipped', t['skipped'], C.COLOR_SKIP),
+                    colorize(u'rescued', t['rescued'], C.COLOR_OK),
+                    colorize(u'ignored', t['ignored'], C.COLOR_WARN),
+                ),
+                screen_only=True
+            )
+
+            self._display.display(
+                u"%s : %s %s %s %s %s %s %s" % (
+                    hostcolor(h, t, False),
+                    colorize(u'ok', t['ok'], None),
+                    colorize(u'changed', t['changed'], None),
+                    colorize(u'unreachable', t['unreachable'], None),
+                    colorize(u'failed', t['failures'], None),
+                    colorize(u'skipped', t['skipped'], None),
+                    colorize(u'rescued', t['rescued'], None),
+                    colorize(u'ignored', t['ignored'], None),
+                ),
+                log_only=True
+            )
+
+        self._display.display("", screen_only=True)
