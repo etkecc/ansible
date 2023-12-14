0369b7586f2805a788c61ea4d700466e91f27a3a
Aine <aine@etke.cc>
2023-12-14 23:55:35 +0200

    trying to fix it

--- bin/ci-notify.sh
+++ bin/ci-notify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-curl -H "Content-Type: application/json" -H "Authorization: Bearer $MATRIX_TOKEN" $MATRIX_URL_FRESH -d "{\"msgtype\":\"m.text\", \"body":\"$CI_COMMIT_MESSAGE\"}"
+curl -H "Content-Type: application/json" -H "Authorization: Bearer $MATRIX_TOKEN" $MATRIX_URL_FRESH -d "{\"msgtype\":\"m.text\", \"body\":\"$CI_COMMIT_MESSAGE\"}"
