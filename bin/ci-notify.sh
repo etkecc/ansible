696808406a7fcbec5810af03714b87ff294a3c4e
Aine <aine@etke.cc>
2023-12-14 23:58:29 +0200

    try again

--- bin/ci-notify.sh
+++ bin/ci-notify.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-curl -H "Content-Type: application/json" -H "Authorization: Bearer $MATRIX_TOKEN" $MATRIX_URL_FRESH -d "{\"msgtype\":\"m.text\", \"body\":\"$CI_COMMIT_MESSAGE\"}"
+curl -H "Content-Type: application/json" -H "Authorization: Bearer $MATRIX_TOKEN" $MATRIX_URL_FRESH -d @- <<EOF
+{
+  "msgtype": "m.text",
+  "body": "$CI_COMMIT_MESSAGE"
+}
+EOF
